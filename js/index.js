8f057240ba8290701c3fb5aa88b55a69944c5d59
Tyson Gushiken <tysonism@gmail.com>
2018-05-17 15:16:32 -0700

    Add modal component to submit button

--- js/index.js
+++ js/index.js
@@ -2,4 +2,10 @@
 // Enable tooltip
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+// Trigger modal on submit, prevent post
+$('#contactSubmitButton').on('click', function(e){
+  e.preventDefault();
+  $('#contactSuccessModal').modal();
+});
