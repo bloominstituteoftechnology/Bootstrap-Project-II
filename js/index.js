0569d7751ed018b35dcc1d8b86400ee7be57b4c4
Tyson Gushiken <tysonism@gmail.com>
2018-05-17 16:05:52 -0700

    Add listener to modal close to reset form

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 // Enable tooltip
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 // Trigger modal on submit, prevent post
@@ -10,3 +10,7 @@ $('#contactForm').submit(function(e) {
   console.log('submitted');
   $('#contactSuccessModal').modal();
 });
+
+$('#contactSuccessModal').on('hidden.bs.modal', function (e) {
+  $('#contactForm')[0].reset();
+})
