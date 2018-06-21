1e7237823322243c95cc0c042b807a2ea0a965b3
AI2EJAY <JHammett1314@gmail.com>
2018-06-21 18:52:39 -0500

    End of day:
    -Didn't get the validation completely working but have browser side validation on the email.

--- js/index.js
+++ js/index.js
@@ -3,3 +3,14 @@ $(function () {
     $('#example').tooltip(options)
   })
   
+var form = document.querySelector('.needs-validation');
+
+form.addEventListener("submit", function (event) {
+    if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+}
+form.classList.add("was-validated");
+
+
+})
