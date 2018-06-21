63b87fc54e03c197370398e17ea1598d1b63af87
Grant Reighard <grantreighard@Grants-iMac.local>
2018-06-21 16:20:08 -0400

    Completed stretch goal.
    Added validation and made fields required.
    
    Required tag overwrites validation in JS, it seems.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 100);
 });
+
+function validateForm() {
+    const x = document.forms["myForm"]["fname"].value;
+    const y = document.forms["myForm"]["fphone"].value;
+    const z = document.forms["myForm"]["femail"].value;
+    if (x === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    if (y === "") {
+        alert("Phone must be filled out");
+        return false;
+    }
+
+    if (z === "") {
+        alert("Email must be filled out");
+        return false;
+    }
+}
