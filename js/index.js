0453d8025ac6ed57fe4ab8649aec80c6333de782
Ashwin <aswinjeganath@gmail.com>
2018-06-22 03:34:55 +0530

    Change style for scroll spy and add smooth scroll

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
+  $('a[href*="#"').click(function(event) {
+    var target = $(this.hash);
+    event.preventDefault();
+    $('html, body').animate(
+      {
+        scrollTop: target.offset().top - 90
+      },
+      1000
+    );
+  });
 });
